9b6898efd778179bd5f3fcda2d1a40950decdb28
Martin Smith <1695515+ms609@users.noreply.github.com>
2021-09-28 14:30:15 +0100

    Reading list

--- assets/js/thisyear.js
+++ assets/js/thisyear.js
@@ -11,3 +11,4 @@ linkLinks('wenlockGroups', 'https://docs.google.com/spreadsheets/d/1ABpa7W4V8ZSt
 linkLinks('wenlockData', 'https://docs.google.com/spreadsheets/d/1WNVOwmHKelVqv0E4LokSrI9LzikeRA_GnRqRQG6mXE4/edit');
 linkLinks('wenlockRubric', 'pdf/WenlockRubric.pdf');
 linkLinks('discussionBoard', 'https://blackboard.durham.ac.uk/ultra/courses/_5402_1/outline/discussion/_90227_1?view=discussions&courseId=_5402_1');
+linkLinks('readingList', 'https://rl.talis.com/3/durham/lists/719102AD-4B96-6917-9F03-5537497D5654.html');
