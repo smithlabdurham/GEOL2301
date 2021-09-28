ed27b423d1fdf1310a8dc77b77ae1350b00c0f66
Martin Smith <1695515+ms609@users.noreply.github.com>
2021-09-28 12:04:40 +0100

    Wenlock-specific tips

--- assets/js/thisyear.js
+++ assets/js/thisyear.js
@@ -8,4 +8,5 @@ linkLinks = function(aClass, aHref) {
 }
 
 linkLinks('wenlockGroups', 'https://docs.google.com/spreadsheets/d/1ABpa7W4V8ZStxXdezYaX3HtMmG7ms5vRleSv88YhuKU/edit');
+linkLinks('wenlockData', 'https://docs.google.com/spreadsheets/d/1WNVOwmHKelVqv0E4LokSrI9LzikeRA_GnRqRQG6mXE4/edit');
 linkLinks('wenlockRubric', 'pdf/WenlockRubric.pdf');
