36816a1ff6c21bd1794be1932820e9be0844a8be
Martin Smith <1695515+ms609@users.noreply.github.com>
2021-09-28 13:27:26 +0100

    thisyear.js

--- assets/js/thisyear.js
+++ assets/js/thisyear.js
@@ -10,3 +10,4 @@ linkLinks = function(aClass, aHref) {
 linkLinks('wenlockGroups', 'https://docs.google.com/spreadsheets/d/1ABpa7W4V8ZStxXdezYaX3HtMmG7ms5vRleSv88YhuKU/edit');
 linkLinks('wenlockData', 'https://docs.google.com/spreadsheets/d/1WNVOwmHKelVqv0E4LokSrI9LzikeRA_GnRqRQG6mXE4/edit');
 linkLinks('wenlockRubric', 'pdf/WenlockRubric.pdf');
+linkLinks('discussionBoard', 'https://blackboard.durham.ac.uk/ultra/courses/_5402_1/outline/discussion/_90227_1?view=discussions&courseId=_5402_1');
