d4ddc6a6e011c14184b0a9e55ddd94778debeba1
Martin Smith <1695515+ms609@users.noreply.github.com>
2021-09-28 08:15:04 +0100

    Wenlock start

---
+++ assets/js/thisyear.js
@@ -0,0 +1,4 @@
+const wenGps = document.getElementsByClassName('wenlockGroups');
+for (var i = 0; i != wenGps.length; i++) {
+  wenGps[i].href = 'https://docs.google.com/spreadsheets/d/1ABpa7W4V8ZStxXdezYaX3HtMmG7ms5vRleSv88YhuKU/edit';
+}
