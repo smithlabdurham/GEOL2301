e0a1b4371c73f832f8191a0eb9777a0db12932ee
Martin Smith <1695515+ms609@users.noreply.github.com>
2021-09-28 08:17:00 +0100

    tab; thisyear.js

--- assets/js/thisyear.js
+++ assets/js/thisyear.js
@@ -1,3 +1,5 @@
+firstClass = new Date("2021-10-07");
+
 const wenGps = document.getElementsByClassName('wenlockGroups');
 for (var i = 0; i != wenGps.length; i++) {
   wenGps[i].href = 'https://docs.google.com/spreadsheets/d/1ABpa7W4V8ZStxXdezYaX3HtMmG7ms5vRleSv88YhuKU/edit';
