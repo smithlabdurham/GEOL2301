186edab7aa2957ca9d86bd4707ee8f5873ee5bfc
Martin Smith <1695515+ms609@users.noreply.github.com>
2021-09-29 11:51:37 +0100

    1SA→1S

--- assets/js/thisyear.js
+++ assets/js/thisyear.js
@@ -14,17 +14,21 @@ for (var i = 0; i != weeks.length; i++) {
   weeks[i].innerHTML = dateI.toDateString();
 }
 
-linkLinks = function(aClass, aHref) {
+const links = {
+  'wenlockGroups': 'https://docs.google.com/spreadsheets/d/1ABpa7W4V8ZStxXdezYaX3HtMmG7ms5vRleSv88YhuKU/edit',
+  'wenlockData': 'https://docs.google.com/spreadsheets/d/1WNVOwmHKelVqv0E4LokSrI9LzikeRA_GnRqRQG6mXE4/edit',
+  'wenlockRubric': 'pdf/WenlockRubric.pdf',
+  'wenlockSlabs': 'https://sketchfab.com/smithlabdurham/collections/wenlock-limestone-samples',
+  'discussionBoard': 'https://blackboard.durham.ac.uk/ultra/courses/_5402_1/outline/discussion/_90227_1?view=discussions&courseId=_5402_1',
+  'blackboard': 'https://blackboard.durham.ac.uk/ultra/courses/_5402_1/outline',
+  'readingList': 'https://rl.talis.com/3/durham/lists/719102AD-4B96-6917-9F03-5537497D5654.html',
+  'jupyter': 'https://notebooks.dmaitre.phyip3.dur.ac.uk/palaeoecosystems-2021/'
+};
+
+for (const [aClass, aHref] of Object.entries(links)) {
   const a = document.getElementsByClassName(aClass);
   for (var i = 0; i != a.length; i++) {
     a[i].href = aHref;
   }
 }
 
-linkLinks('wenlockGroups', 'https://docs.google.com/spreadsheets/d/1ABpa7W4V8ZStxXdezYaX3HtMmG7ms5vRleSv88YhuKU/edit');
-linkLinks('wenlockData', 'https://docs.google.com/spreadsheets/d/1WNVOwmHKelVqv0E4LokSrI9LzikeRA_GnRqRQG6mXE4/edit');
-linkLinks('wenlockRubric', 'pdf/WenlockRubric.pdf');
-linkLinks('wenlockSlabs', 'https://sketchfab.com/smithlabdurham/collections/wenlock-limestone-samples');
-linkLinks('discussionBoard', 'https://blackboard.durham.ac.uk/ultra/courses/_5402_1/outline/discussion/_90227_1?view=discussions&courseId=_5402_1');
-linkLinks('blackboard', 'https://blackboard.durham.ac.uk/ultra/courses/_5402_1/outline');
-linkLinks('readingList', 'https://rl.talis.com/3/durham/lists/719102AD-4B96-6917-9F03-5537497D5654.html');
